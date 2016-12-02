1099a0962722d513291b3eeaf917ee0737035f47
Jacob Vigeveno <vigevenoj@gmail.com>
2016-12-01 23:27:56 -0800

    Set up fetch cycle

--- src/main/java/com/sharkbaitextraordinaire/isstracker/IssLocationFetcher.java
+++ src/main/java/com/sharkbaitextraordinaire/isstracker/IssLocationFetcher.java
@@ -3,7 +3,10 @@ package com.sharkbaitextraordinaire.isstracker;
 import java.io.IOException;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.Response;
@@ -16,7 +19,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class IssLocationFetcher implements Runnable {
+public class IssLocationFetcher {
 
 	// TODO set up so end user can pick one or the other or an alternate api
 	private static final String OPEN_NOTIFY_API_URL = "http://api.open-notify.org/iss-now.json";
@@ -25,6 +28,8 @@ public class IssLocationFetcher implements Runnable {
 	
 	private static int POLL_INTERVAL = 30; // only check every 30 seconds
 	private static DecimalFormat df = new DecimalFormat("#.####");
+	private final ScheduledExecutorService scheduler =
+		     Executors.newScheduledThreadPool(1);
 	
 	
 	// We'll push updates onto this for a consumer to pop off as they wish
@@ -40,6 +45,9 @@ public class IssLocationFetcher implements Runnable {
 		
 		// Set up rounding for lat/lng
 		df.setRoundingMode(RoundingMode.HALF_UP); 
+		scheduler.scheduleAtFixedRate(() -> {
+			fetch();
+		}, 0, 30, TimeUnit.SECONDS);
 	}
 	
 	public void fetch() {
@@ -67,9 +75,7 @@ public class IssLocationFetcher implements Runnable {
 		}
 	}
 	
-	public void run() {
-		
-	}
+	
 	
 	
 }
