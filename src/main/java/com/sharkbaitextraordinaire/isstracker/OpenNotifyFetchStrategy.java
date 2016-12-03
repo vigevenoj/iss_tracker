21ebafc5c4222baa92445d8a73e727502cd9d158
Jacob Vigeveno <vigevenoj@gmail.com>
2016-12-02 18:15:30 -0800

    Fix formatting

--- src/main/java/com/sharkbaitextraordinaire/isstracker/OpenNotifyFetchStrategy.java
+++ src/main/java/com/sharkbaitextraordinaire/isstracker/OpenNotifyFetchStrategy.java
@@ -4,12 +4,12 @@ public class OpenNotifyFetchStrategy implements IssLocationFetchStrategy {
 
 	private static final String OPEN_NOTIFY_API_URL = "http://api.open-notify.org/iss-now.json";  
 
-  public String getApiUrl() {
-    return OPEN_NOTIFY_API_URL;
-  }
+	public String getApiUrl() {
+		return OPEN_NOTIFY_API_URL;
+	}
 
-  public int getPollInterval() {
-    return 30;
-  }
+	public int getPollInterval() {
+		return 30;
+	}
 
 }
