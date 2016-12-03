2ace2139aa318d80759256625663ebcdeaf4fca6
Jacob Vigeveno <vigevenoj@gmail.com>
2016-12-02 20:32:42 -0800

    Fix indentation

--- src/main/java/com/sharkbaitextraordinaire/isstracker/IssLocationFetcher.java
+++ src/main/java/com/sharkbaitextraordinaire/isstracker/IssLocationFetcher.java
@@ -12,60 +12,61 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.client.*;
-import org.geojson.*;
-
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * 
+ * @author vigevenoj
+ *
+ */
 public class IssLocationFetcher {
 
 	// TODO set up so end user can pick one or the other or an alternate api
 	private static final String OPEN_NOTIFY_API_URL = "http://api.open-notify.org/iss-now.json";
 	private static final String WHERETHEISSAT_URL = "http://wheretheiss.at/w/developer";
 	private static ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-	
+
 	private static int POLL_INTERVAL = 30; // only check every 30 seconds
 	private static DecimalFormat df = new DecimalFormat("#.####");
 	private final ScheduledExecutorService scheduler =
-		     Executors.newScheduledThreadPool(1);
-	
-	
+			Executors.newScheduledThreadPool(1);
+
+
 	// We'll push updates onto this for a consumer to pop off as they wish
 	private LinkedBlockingQueue<IssLocation> queue;
 	private JerseyClient client;
-	
-  public IssLocationFetcher() {
-    // If a queue isn't provided, set one up
-    this.queue = new LinkedBlockingQueue<IssLocation>();
-  }
+
+	public IssLocationFetcher() {
+		// If a queue isn't provided, set one up
+		this.queue = new LinkedBlockingQueue<IssLocation>();
+	}
 
 	public IssLocationFetcher(LinkedBlockingQueue<IssLocation> queue) {
 		this.queue = queue;
-		
-		
+
+
 		ClientConfig configuration = new ClientConfig();
 		client = JerseyClientBuilder.createClient(configuration);
-		
+
 		// Set up rounding for lat/lng
 		df.setRoundingMode(RoundingMode.HALF_UP); 
 
-    // TODO make the interval configurable
+		// TODO make the interval configurable
 		scheduler.scheduleAtFixedRate(() -> {
 			fetch();
 		}, 0, 30, TimeUnit.SECONDS);
 	}
-	
+
 	public void fetch() {
 		JerseyWebTarget target = client.target(OPEN_NOTIFY_API_URL);
 		Invocation.Builder invocationBuilder = target.request();
 		Response response = invocationBuilder.get();
-		
+
 		int status = response.getStatus();
 		if (status == 200) {
 			String responseString = response.readEntity(String.class);
-			
+
 			System.out.println(responseString);
 			try {
 				IssLocation issLocation = mapper.readValue(responseString, IssLocation.class);
@@ -81,8 +82,8 @@ public class IssLocationFetcher {
 			}
 		}
 	}
-	
-	
-	
-	
+
+
+
+
 }
