2bd1530480d7e21a043abf829692deead53b261a
Jacob Vigeveno <vigevenoj@gmail.com>
2016-12-02 18:16:00 -0800

    Fix formatting

--- src/main/java/com/sharkbaitextraordinaire/isstracker/IssLocationFetchStrategy.java
+++ src/main/java/com/sharkbaitextraordinaire/isstracker/IssLocationFetchStrategy.java
@@ -2,14 +2,14 @@ package com.sharkbaitextraordinaire.isstracker;
 
 public interface IssLocationFetchStrategy {
 
-  /**
-   * The url which will be requested to fetch the ISS location
-   */
-  public String getApiUrl();
+	/**
+	 * The url which will be requested to fetch the ISS location
+	 */
+	public String getApiUrl();
 
-  /** 
-   * The number of seconds this strategy waits between requests for an update from the API
-   */
-  public int getPollInterval();
+	/** 
+	 * The number of seconds this strategy waits between requests for an update from the API
+	 */
+	public int getPollInterval();
 
 }
